d9d246c297214fda90adf8ba9c33c92596a88b23
LuisPeralta <luis.peralta@dycsi.net>
2024-05-13 12:45:10 -0600

    Segundo commit con resto de consultas embebidas Update y Delete y pruebas locales con BD Postman

--- srv/contracts.cds
+++ srv/contracts.cds
@@ -3,5 +3,7 @@ using {com.finobankdigital as finobankdigital} from '../db/schema_contracts';
 service ManageContracts {
 
     entity GetContracts   as projection on finobankdigital.Contracts;
-    entity CreateContract as projection on finobankdigital.Contracts
+    entity CreateContract as projection on finobankdigital.Contracts;
+    entity UpdateContract as projection on finobankdigital.Contracts;
+    entity DeleteContract as projection on finobankdigital.Contracts;
 }
--- srv/contracts.js
+++ srv/contracts.js
@@ -1,15 +1,17 @@
 const cds = require("@sap/cds");
 const req = require("express/lib/request");
 const { Contracts } = cds.entities("com.finobankdigital");
+
 module.exports = (srv) => {
   //***************READ*****************//
   srv.on("READ", "GetContracts", async (req) => {
     if (req.data.Ranl1 !== undefined) {
-        return await SELECT.from`com.finobankdigital.Contracts`
+      return await SELECT.from`com.finobankdigital.Contracts`
         .where`Ranl1 = ${req.data.Ranl1}`;
-    }  
+    }
     return await SELECT.from(Contracts);
   });
+
   //***************CREATE***************//
   srv.on("CREATE", "CreateContract", async (req) => {
     let returnData = await cds
@@ -17,6 +19,11 @@ module.exports = (srv) => {
       .run(
         INSERT.into(Contracts).entries({
           Ranl1: req.data.Ranl1,
+          Bukrs: req.data.Bukrs,
+          Rbear: req.data.Rbear,
+          Gsart: req.data.Gsart,
+          Dblfz: req.data.Bblfz,
+          Delfz: req.data.Delfz,
         })
       )
       .then((resolve, reject) => {
@@ -36,5 +43,58 @@ module.exports = (srv) => {
     console.log("Before End", returnData);
     return returnData;
   });
-};  
 
+  //***************UPDATE***************//
+  srv.on("UPDATE", "UpdateContract", async (req) => {
+    let returnData = await cds
+      .transaction(req)
+      .run([
+        UPDATE(Contracts, req.data.Ranl1).set({
+          Bukrs: req.data.Bukrs,
+          Rbear: req.data.Rbear,
+          Gsart: req.data.Gsart,
+          Dblfz: req.data.Dblfz,
+          Delfz: req.data.Delfz,
+        }),
+      ])
+      .then((resolve, reject) => {
+        console.log("Resolve: ", resolve);
+        console.log("Reject: ", reject);
+
+        if (resolve[0] == 0) {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        req.error(err.code, err.message);
+      });
+    console.log("Before end", returnData);
+    return returnData;
+  });
+  
+  //***************DELETE***************//
+  srv.on("DELETE", "DeleteContract", async (req) => {
+    let returnData = await cds
+      .transaction(req)
+      .run(
+        DELETE.from(Contracts).where({
+          Ranl1: req.data.Ranl1,
+        })
+      )
+      .then((resolve, reject) => {
+        console.log("Resolve", resolve);
+        console.log("Reject", reject);
+
+        if (resolve !== 1) {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        req.error(err.code, err.message);
+      });
+    console.log("Before End", returnData);
+    return await returnData;
+  });
+};
